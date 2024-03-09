652e3932beb90ddc48138e7ed3d9176dc0953ae4
Sulabh Shrestha <shresthasulabh777@gmail.com>
2024-03-10 00:22:02 +0545

    ResultCert: Added pdf viewer

--- src/components/ResultCert.jsx
+++ src/components/ResultCert.jsx
@@ -1,5 +1,31 @@
+import PureModal from "react-pure-modal";
+import "react-pure-modal/dist/react-pure-modal.min.css";
+import ResultCertPdfPreviewer from "./ResultCertPdfPreviewer";
+
+
 function ResultCert() {
-  return <></>;
+  return <>
+
+  <h1>Hello</h1>
+  
+  <div className="flex flex-col">
+      <PureModal
+        header="Custom heading"
+        width="80vw"
+        className=""
+        scrollable={false}
+        isOpen={true}
+        closeButton="close"
+        onClose={() => {
+          
+          return true;
+        }}
+      >
+        {/* Content */}
+        <ResultCertPdfPreviewer/>
+      </PureModal>
+    </div>
+  </>;
 }
 
 export default ResultCert;
